d75e10ac528080f823a86906be9acb08919539be
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-01-23 04:00:15 -0800

    Rename icon_url to iconUrl

--- src/lib/components/IconLink.svelte
+++ src/lib/components/IconLink.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
     interface Props {
         url: string;
-        icon_url: string;
+        iconUrl: string;
         alt?: string;
         animateHover?: boolean;
         color?: boolean;
     }
 
-    let { url, icon_url, alt = "", animateHover = true, color = false }: Props = $props();
+    let { url, iconUrl, alt = "", animateHover = true, color = false }: Props = $props();
 </script>
 
 <a href={url} target="_blank" rel="nofollow" class="w-9 h-9 rounded-md select-none">
     <img
-        src={icon_url}
+        src={iconUrl}
         {alt}
         class={[
             "shrink-0 transition",
