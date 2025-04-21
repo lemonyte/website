481b0cb864b2a82f0aedaa228af66e2fd713fcc1
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-04-21 02:21:43 -0700

    Add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import Island from "$lib/components/Island.svelte";
+    import { page } from "$app/state";
+</script>
+
+<div class="h-screen flex flex-col justify-center">
+    <Island>
+        <div class="flex flex-col items-center justify-center h-[50vh]">
+            <div class="flex flex-col gap-4">
+                <h1 class="flex flex-col">
+                    <span class="text-9xl font-semibold">{page.status}</span>
+                    <span class="text-2xl">{page.error?.message}</span>
+                </h1>
+                <a href="/" class="text-sky-500 hover:underline">Go back home</a>
+            </div>
+        </div>
+    </Island>
+</div>
