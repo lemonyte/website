13056e625ed44266b352881b5af0a793ef1debf4
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-04-26 04:05:06 -0700

    Adjust reading width

--- src/lib/components/Island.svelte
+++ src/lib/components/Island.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="flex flex-col items-center">
-    <div class="w-11/12 md:w-2/3 p-6 backdrop-blur-3xl rounded-xl bg-neutral-100/40 dark:bg-neutral-900/40">
+    <div class="w-11/12 md:w-2/3 xl:w-1/2 p-6 backdrop-blur-3xl rounded-xl bg-neutral-100/40 dark:bg-neutral-900/40">
         {@render children()}
     </div>
 </div>
