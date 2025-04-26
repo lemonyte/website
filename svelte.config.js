149fe6ff40ce5a239164489132b21c110b37e4d2
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-04-26 04:18:42 -0700

    Switch to CF pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
 
-const dev = process.env.NODE_ENV === "development";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,9 +10,6 @@ const config = {
     preprocess: [vitePreprocess(), mdsvex()],
     kit: {
         adapter: adapter(),
-        paths: {
-            base: dev ? "" : process.env.BASE_PATH,
-        },
     },
 };
 
