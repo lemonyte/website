574d60cdc4bfec833dbc16884deedfcaf68ffa5a
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-04-26 04:59:33 -0700

    Use R2 bucket for media

---
+++ src/routes/assets/[path=file]/+server.ts
@@ -0,0 +1,17 @@
+import type { RequestHandler } from "./$types";
+
+export const GET = (async ({ platform, params }) => {
+    if (platform === undefined) {
+        return new Response("Internal Server Error", { status: 500 });
+    }
+    const obj = await platform.env.MEDIA.get(params.path);
+    if (obj === null) {
+        return new Response("Not Found", { status: 404 });
+    }
+    return new Response(obj.body, {
+        headers: {
+            "Content-Type": obj.httpMetadata?.contentType || "image/jpeg",
+            "Cache-Control": "max-age=604800",
+        },
+    });
+}) satisfies RequestHandler;
