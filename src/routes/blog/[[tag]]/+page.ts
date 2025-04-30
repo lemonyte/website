c341b957d3171482891b8e4abfe23a19b8aebf7e
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-04-29 18:49:05 -0700

    Update error message

--- src/routes/blog/[[tag]]/+page.ts
+++ src/routes/blog/[[tag]]/+page.ts
@@ -6,7 +6,7 @@ export const load = ({ params }) => {
     if (tag) {
         const filteredPosts = posts.filter((post) => post.tags.includes(tag.toLowerCase()));
         if (filteredPosts.length === 0) {
-            throw error(404, `Tag not found: ${tag}`);
+            throw error(404, `Post or tag not found: ${tag}`);
         }
         return { tag, posts: filteredPosts };
     }
