88ca4ea5d9fbab2ce03854dccf4ed5f4a153a605
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-05-02 23:06:53 -0700

    Add heading links

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,20 @@
 import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { mdsvex } from "mdsvex";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
     extensions: [".svelte", ".svx"],
-    preprocess: [vitePreprocess(), mdsvex()],
+    preprocess: [
+        vitePreprocess(),
+        mdsvex({
+            rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
+        }),
+    ],
     kit: {
         adapter: adapter(),
     },
