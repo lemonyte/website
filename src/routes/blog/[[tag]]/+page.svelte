e74d6e8606733e30f9079bdfad127ef630fb8dee
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-05-13 23:17:59 -0700

    Increase blog page heading size

--- src/routes/blog/[[tag]]/+page.svelte
+++ src/routes/blog/[[tag]]/+page.svelte
@@ -9,7 +9,7 @@
 <Head title={data.tag ? `Posts on ${data.tag}` : "Posts"} description="Lemonyte's Blog" />
 
 <Island>
-    <h1 class="text-2xl mb-4">
+    <h1 class="text-4xl font-semibold mb-4">
         {#if data.tag}
             Posts on <span class="capitalize">{data.tag}</span>
         {:else}
