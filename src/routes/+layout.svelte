2a6c32152c01ea260d2f08608dbbae825b1c13a1
lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-10-30 02:29:38 +0100

    Make title text link to home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
         <Island>
             <div class="flex flex-col sm:flex-row gap-4 sm:items-center">
                 <span class="flex flex-row items-center">
-                    <span class="font-semibold text-xl opacity-60">Lemonyte</span>
+                    <a href="/" class="link font-semibold text-xl">Lemonyte</a>
                     <button
                         class={[
                             "inline-flex items-center justify-center sm:hidden text-2xl ml-auto",
@@ -29,8 +29,6 @@
                     </button>
                 </span>
                 <div class={["flex flex-col sm:flex-row gap-2 sm:flex sm:ml-auto", { hidden: !mobileNavOpen }]}>
-                    <a href="/" class="link">Home</a>
-                    <span class="hidden sm:inline opacity-50">•</span>
                     <a href="/blog" class="link">Blog</a>
                     <span class="hidden sm:inline opacity-50">•</span>
                     <a href="/challenges" class="link">CTF Challenges</a>
