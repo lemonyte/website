fdcfe987e70d2ebc0dc7a2ec81ad6acdca80cdbd
lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-10-30 03:09:08 +0100

    Format long lines

--- src/lib/components/Island.svelte
+++ src/lib/components/Island.svelte
@@ -9,7 +9,12 @@
 </script>
 
 <div class="flex flex-col items-center">
-    <div class="w-full md:w-2/3 xl:w-1/2 p-4 md:p-6 backdrop-blur-3xl rounded-xl bg-neutral-100/40 dark:bg-neutral-900/40">
+    <div
+        class={[
+            "w-full md:w-2/3 xl:w-1/2 p-4 md:p-6 backdrop-blur-3xl rounded-xl",
+            "bg-neutral-100/40 dark:bg-neutral-900/40",
+        ]}
+    >
         {@render children()}
     </div>
 </div>
--- src/lib/components/TocIsland.svelte
+++ src/lib/components/TocIsland.svelte
@@ -11,10 +11,14 @@
 <div class="absolute right-4 h-full select-none hidden xl:block w-1/5">
     <!--
         The `prose` and `not-prose` classes in the same class list is intentional.
-        We want the typography variables to be available in the TOC but without applying all the default typography styling.
+        We want the typography variables to be available in the TOC
+        but without applying all the default typography styling.
     -->
     <div
-        class="p-2 backdrop-blur-3xl rounded-xl bg-neutral-100/40 dark:bg-neutral-900/40 sticky top-4 prose not-prose prose-neutral dark:prose-invert"
+        class={[
+            "p-2 backdrop-blur-3xl rounded-xl bg-neutral-100/40 dark:bg-neutral-900/40",
+            "sticky top-4 prose not-prose prose-neutral dark:prose-invert",
+        ]}
     >
         {@render children()}
     </div>
