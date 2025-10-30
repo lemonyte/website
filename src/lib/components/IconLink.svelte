1523c237c1c67f6d72c5230792e76689301d9124
lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-10-30 03:49:40 +0100

    Make icon link size configurable

--- src/lib/components/IconLink.svelte
+++ src/lib/components/IconLink.svelte
@@ -5,12 +5,22 @@
         alt?: string;
         animateHover?: boolean;
         color?: boolean;
+        width?: string;
+        height?: string;
     }
 
-    const { url, iconUrl, alt = "", animateHover = true, color = false }: Props = $props();
+    const {
+        url,
+        iconUrl,
+        alt = "",
+        animateHover = true,
+        color = false,
+        width = "36px",
+        height = "36px",
+    }: Props = $props();
 </script>
 
-<a href={url} target="_blank" rel="nofollow" class="w-9 h-9 rounded-md select-none flex items-center">
+<a href={url} target="_blank" rel="nofollow" class="rounded-md select-none flex items-center">
     <img
         src={iconUrl}
         {alt}
@@ -18,5 +28,7 @@
             "shrink-0 transition duration-75",
             animateHover && (color ? "saturate-0 hover:saturate-100" : "opacity-50 hover:opacity-100"),
         ]}
+        {width}
+        {height}
     />
 </a>
