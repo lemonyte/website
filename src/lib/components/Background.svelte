6724b076248dfd8acea14b813ad2f1b46de38ac9
lemonyte <49930425+lemonyte@users.noreply.github.com>
2026-01-01 21:45:28 +0100

    sanity check: remove bg blur

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -35,6 +35,6 @@
     };
 </script>
 
-<Canvas autoplay class="w-full h-full fixed top-0 left-0 -z-20 blur-2xl">
+<Canvas autoplay class="w-full h-full fixed top-0 left-0 -z-20">
     <Layer {render} />
 </Canvas>
