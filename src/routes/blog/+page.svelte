27fa9dbf61aad96704d7598e1081eb8e7e4cfdda
lemonyte <49930425+lemonyte@users.noreply.github.com>
2026-05-07 17:39:46 +0200

    URI-encode tags in query params

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -6,8 +6,8 @@
     import { page } from "$app/state";
     import { posts, tags } from "$lib/posts";
 
-    let tag = $derived(page.url.searchParams.get("tag"));
-    let filteredPosts = $derived(posts.filter((post) => (tag ? post.tags.includes(tag) : true)));
+    let tagFilter = $derived(page.url.searchParams.get("tag"));
+    let filteredPosts = $derived(posts.filter((post) => (tagFilter ? post.tags.includes(tagFilter) : true)));
 </script>
 
 <Head title="Posts" description="Lemonyte's Blog" />
@@ -16,12 +16,12 @@
     <h1 class="text-4xl font-semibold mb-4">Posts</h1>
     <div class="flex gap-2 flex-wrap mb-4 select-none">
         <a href="/blog">
-            <Tag hover={true} selected={!tag}>All</Tag>
+            <Tag hover={true} selected={!tagFilter}>All</Tag>
         </a>
-        {#each tags as t}
-            <a href={`/blog?tag=${t}`}>
-                <Tag hover={true} selected={tag === t}>
-                    {t}
+        {#each tags as tag}
+            <a href={`/blog?tag=${encodeURIComponent(tag)}`}>
+                <Tag hover={true} selected={tag === tagFilter}>
+                    {tag}
                 </Tag>
             </a>
         {/each}
